34244bc5fb701f870036430c44842bf09db13bab
swolecoder <swolecoder@gmail.com>
2020-10-21 22:46:59 -0400

    D ay10

--- Day10/maxSumDivThree.js
+++ Day10/maxSumDivThree.js
@@ -2,16 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSumDivThree = function(A) {
+var maxSumDivThree = function(nums) {
 
 
-    let N = A.length;
+    let N = nums.length;
     let cur = Array(3).fill(0);
     for (let i = 1; i <= N; ++i) {
-        let pre = [...cur];
+        let pre = [...cur]; // create current from previous 🤔
         for (let j of [0, 1, 2]) {
-            let sum = A[i - 1] + pre[j];
-            cur[sum % 3] = Math.max(cur[sum % 3], sum);
+            let sum = nums[i - 1] + pre[j]; // add A[i] onto each previous bucket (A[i - 1] for A[i] because buckets is offset by 1)
+            cur[sum % 3] = Math.max(cur[sum % 3], sum); // update each (current sum % 3) bucket to max of itself and the current sum
+        }
     }
-    return cur[0]; /
+    return cur[0]; // max sum of all N items of A which is evenly divisible by 3 🎯
 };
