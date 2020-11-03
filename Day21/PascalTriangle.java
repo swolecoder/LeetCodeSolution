6f878c8c72fece0c9eefcd7ab281711a1aed1eb1
Sathish Kumar Manoharan <2contactsathish@gmail.com>
2020-11-03 09:57:05 -0800

    Create PascalTriangle.java

---
+++ Day21/PascalTriangle.java
@@ -0,0 +1,22 @@
+// calculate element value: triangle(i)(j)=triangle(i-1)(j-1)+triangle(i-1)(j) except for the first and last element
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        if (numRows <=0){
+            return triangle;
+        }
+        for (int i=0; i<numRows; i++){
+            List<Integer> row =  new ArrayList<Integer>();
+            for (int j=0; j<i+1; j++){
+                if (j==0 || j==i){
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
