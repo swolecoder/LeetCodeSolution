9ed69672b34cf8d5c83aa67e55bb39e234ccddc7
swolecoder <swolecoder@gmail.com>
2020-12-31 15:21:07 -0500

    Day83

--- Day83/index.js
+++ Day83/index.js
@@ -2,16 +2,16 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(h) {
+var largestRectangleArea = function (h) {
     let area = 0;
 
-    if(h.length == 1)return h[0];
+    if (h.length == 1) return h[0];
 
-    for(let i=0; i < h.length; i++){
+    for (let i = 0; i < h.length; i++) {
         let currH = h[i]
-        for(let j = i; j < h.length; j++){
+        for (let j = i; j < h.length; j++) {
             currH = Math.min(currH, h[j]);
-            area = Math.max(area,currH * (j-i+1) )
+            area = Math.max(area, currH * (j - i + 1))
         }
     }
 
@@ -24,24 +24,24 @@ var largestRectangleArea = function(h) {
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
+var largestRectangleArea = function (heights) {
 
     let area = 0;
     let stack = [];
     heights.push(0);
     heights.unshift(0);
 
-    for(let i=0; i < heights.length; i++){
+    for (let i = 0; i < heights.length; i++) {
         let curr = heights[i];
 
-        while(stack &&  heights[stack[stack.length-1]] > curr){
+        while (stack && heights[stack[stack.length - 1]] > curr) {
             let j = stack.pop();
             let width;
 
-            if(stack.length==0){
-                width =1
-            }else{
-                width = i - stack[stack.length-1] -1;
+            if (stack.length == 0) {
+                width = 1
+            } else {
+                width = i - stack[stack.length - 1] - 1;
             }
 
             area = Math.max(area, width * heights[j])
